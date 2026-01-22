1b2c7a43a0b28770fb3aa37eecc4df4b8ffd0510
Andrei Petro Matos Leal <andrei18l@hotmail.com>
2026-01-22 20:31:09 -0300

    chore: projeto inicial NutriFit (arquitetura SaaS)

---
+++ src/test/java/com/andreileal/app/nutrifit/NutrifitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreileal.app.nutrifit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrifitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
